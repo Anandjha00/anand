98c5857cb84ad638302b323ab79e7ce52a59f20e
Anand Kumar Jha <lenovopc354@gmail.com>
2024-09-23 16:03:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("menu-icon").addEventListener("click", function() {
+    var navList = document.getElementById("nav-list");
+    navList.classList.toggle("show"); // Toggle the 'show' class to display the menu
+});
